e388ee69c26f7bd691781161c2d833715a5726f0
Duany Dreyton <duanyrf@gmail.com>
2020-11-09 00:51:02 -0300

    :sparkles: challenge 06 finalized

--- src/database/migrations/1604800010966-CreateTransaction.ts
+++ src/database/migrations/1604800010966-CreateTransaction.ts
@@ -29,7 +29,9 @@ export default class CreateTransaction1604800010966
           },
           {
             name: 'value',
-            type: 'integer',
+            type: 'decimal',
+            precision: 10,
+            scale: 2,
           },
           {
             name: 'category_id',
--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,81 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import { getCustomRepository, getRepository, In, Not } from 'typeorm';
+import Category from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface CSVTransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  categoriesRepository = getRepository(Category);
+
+  transactionsRepository = getCustomRepository(TransactionsRepository);
+
+  async execute(filePath: string): Promise<Transaction[]> {
+    const contactsReadStream = fs.createReadStream(filePath);
+
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const transactions: CSVTransaction[] = [];
+    const categories: string[] = [];
+
+    const parseCSV = contactsReadStream.pipe(parsers).on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const categoriesInDB = await this.categoriesRepository.find({
+      where: { title: In(categories) },
+    });
+
+    const newCategoriesTitle = categoriesInDB.map(
+      (category: Category) => category.title,
+    );
+
+    const newCategoriesToSave = categories
+      .filter(category => !newCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = this.categoriesRepository.create(
+      newCategoriesToSave.map(title => ({ title })),
+    );
+
+    await this.categoriesRepository.save(newCategories);
+
+    const allCategories = [...categoriesInDB, ...newCategories];
+
+    const newTransactions = this.transactionsRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: allCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await this.transactionsRepository.save(newTransactions);
+
+    await fs.promises.unlink(filePath);
+
+    return newTransactions;
   }
 }
 
